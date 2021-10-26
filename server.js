802e8049d77cd85ff99cba492848a7c6534175b4
Owen Utley <owen.utley17@gmail.com>
2021-10-26 15:38:28 -0600

    features removed

--- server.js
+++ server.js
@@ -30,28 +30,6 @@ app.get('/test', (req, res) => {
       }
 })
 
-let students = []
-
-app.post('/api/student', (req, res)=>{
-    let {name} = req.body
-    name = name.trim()
-
-    const index = students.findIndex(studentName => studentName === name)
-
-    if(index === -1 && name !== ""){
-        students.push(name)
-    rollbar.log('student added successfully', {
-        auth: 'Owen', type: 'manual entry'})
-    res.status(200).send(students)
-    } else if (name === ''){
-        rollbar.error('No name given')
-        res.status(400).send('Must provide a name.')
-    } else {
-        rollbar.warning('Name already exists')
-        res.status(400).send('That student already exists.')
-    }
-})
-
 app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4765
