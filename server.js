bc88efe3890dd12ff72922e0cb683223dbbd48e6
Owen Utley <owen.utley17@gmail.com>
2021-10-26 14:13:36 -0600

    server initalized

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const path = require('path')
-const Rollbar = require('rollbar')
+// const Rollbar = require('rollbar')
 
 const app = express()
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    // rollbar.info("html file served successfully")
+})
 
-app.use(rollbar.errorHandler())
+
+// app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4765
 
